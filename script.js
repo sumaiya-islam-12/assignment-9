2e26bac04e037cf116ec8ea7418d7c9b770efe44
Your Name <sumaiyaislam9997@gmail.com>
2024-10-16 10:10:12 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+
+// const numbers =[1, 5, 6, 40, 45, 67, 90]
+// console.log(numbers.length);       /// cheak length
+
+
+
+
+// console.log(numbers[4]);         /// cheack index
+
+
+
+// console.log(numbers.indexOf(67)); /// cheack indexof
+
+
+
+
+// numbers.push(76);                  /// cheack push
+// console.log(numbers);
+
+
+
+
+// numbers.pop("70");              /// cheack pop
+// console.log(numbers);
+
+
+
+const team = ["Sumaiya","Sumu","Shanti","Sumoo","sulu"]  /// cheack shift
+// team.shift()
+// console.log(team);
+
+
+
+// team.unshift("jannat")        /// cheack unshift
+// console.log(team);
+
+
+
+// team.push("limu");          /// cheack push
+// console.log(team);
+
+
+
+
+team.pop("Sulu");         /// cheak pop
+console.log(team);
+
+
+
+
+
+console.log(team.includes("Sumaiya"));  /// cheack includes
+console.log(team.includes("Ash"));
+console.log(Array.isArray(team));
+
